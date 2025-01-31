6261db5d34f154225d46782de1101ab061a8f140
Abid Erfan <abid_erfan7@yahoo.com>
2025-01-31 15:23:30 +0100

    Add files via upload

---
+++ GrassauTime/src/main/java/com/grassau/GrassauTime/GrassauTimeApplication.java
@@ -0,0 +1,13 @@
+package com.grassau.GrassauTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrassauTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrassauTimeApplication.class, args);
+	}
+
+}

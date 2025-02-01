bb850527e807e2560590d7d214229e5c480923d6
Abid Erfan <abid_erfan7@yahoo.com>
2025-02-01 14:16:24 +0100

    Delete GrassauTime/src/main/db/repositories directory

--- GrassauTime/src/main/db/repositories/WorkingLogsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grassau.GrassauTime.db.repositories;
-
-import com.grassau.GrassauTime.db.entities.WorkingLogs;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WorkingLogsRepository extends CrudRepository<WorkingLogs, Integer> {
-}

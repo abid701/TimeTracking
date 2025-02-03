e0e427f9d4589eb58186fc37368cc7884d3c2253
Abid Erfan <abid_erfan7@yahoo.com>
2025-02-03 23:16:05 +0100

    Create TasksController.java

---
+++ GrassauTime/src/main/java/com/grassau/GrassauTime/web/controllers/TasksController.java
@@ -0,0 +1,16 @@
+package com.grassau.GrassauTime.web.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TasksController {
+
+    @GetMapping("/tasks")
+    public String showTask(Model model){
+
+        return "view/tasks.html";
+    }
+}

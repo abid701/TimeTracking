7b744a1d47bba9f7954ba3080da462abe887c5a9
Abid Erfan <abid_erfan7@yahoo.com>
2025-02-03 23:10:41 +0100

    Update WorkingLogsRepository.java

--- GrassauTime/src/main/java/com/grassau/GrassauTime/db/repositories/WorkingLogsRepository.java
+++ GrassauTime/src/main/java/com/grassau/GrassauTime/db/repositories/WorkingLogsRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WorkingLogsRepository extends CrudRepository<WorkingLogs, Integer> {
+   // List<Employees> findAllEmployessFirstName();
 }

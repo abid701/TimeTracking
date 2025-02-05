b6b2c7716a9f1e3629b4a792b6f9c349c6f933ba
Abid Erfan <abid_erfan7@yahoo.com>
2025-02-05 10:12:06 +0100

    Delete GrassauTime directory

--- GrassauTime/src/main/java/com/grassau/GrassauTime/db/repositories/EmployeesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grassau.GrassauTime.db.repositories;
-
-import com.grassau.GrassauTime.db.entities.Employees;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeesRepository extends CrudRepository<Employees, Integer> {
-}
--- GrassauTime/src/main/java/com/grassau/GrassauTime/web/controllers/ProjectsController.java
+++
@@ -1,7 +0,0 @@
-package com.grassau.GrassauTime.web.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class ProjectsController {
-}

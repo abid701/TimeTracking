61138de2367ed40d53ed03feadf041003ecf6be6
Abid Erfan <abid_erfan7@yahoo.com>
2025-03-05 22:46:05 +0100

    Update WorkingLogRepository.java

--- GrassauTime/src/main/java/com/grassau/GrassauTime/db/repositories/WorkingLogRepository.java
+++ GrassauTime/src/main/java/com/grassau/GrassauTime/db/repositories/WorkingLogRepository.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.Optional;
 
 @Repository
@@ -18,5 +19,6 @@ public interface WorkingLogRepository extends JpaRepository<WorkingLog, Integer>
     Optional<WorkingLog> findByCardNumberAndWorkDate(String cardNumber, LocalDate date);
     Iterable<WorkingLog> findAllByWorkDate(LocalDate date);
     WorkingLog findByEmployeeAndWorkDate(Employee employee,LocalDate date);
+    Iterable<WorkingLog> findAllByWorkDateBetween(LocalDate startDate, LocalDate endDate);
 }
 

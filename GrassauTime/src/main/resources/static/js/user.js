ff2ff63b9c02fc2f268aa0127806862d0bbf53d6
Abid Erfan <abid_erfan7@yahoo.com>
2025-03-05 13:04:14 +0100

    Update user.js

--- GrassauTime/src/main/resources/static/js/user.js
+++ GrassauTime/src/main/resources/static/js/user.js
@@ -1,9 +1,13 @@
+  function toggleSettings() {
+      document.querySelector(".settings-menu").classList.toggle("show");
+  }
+
 // Open the modal
 function openModal() {
-    document.getElementById("employeeModal").style.display = "flex";
+    document.getElementById("userModal").style.display = "flex";
 }
 
 // Close the modal
 function closeModal() {
-    document.getElementById("employeeModal").style.display = "none";
+    document.getElementById("userModal").style.display = "none";
 }

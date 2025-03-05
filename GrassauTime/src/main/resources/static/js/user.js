2a00a5f68f4ee4a7499e27504c142dfc2b316284
Abid Erfan <abid_erfan7@yahoo.com>
2025-03-05 12:20:18 +0100

    Create user.js

---
+++ GrassauTime/src/main/resources/static/js/user.js
@@ -0,0 +1,9 @@
+// Open the modal
+function openModal() {
+    document.getElementById("employeeModal").style.display = "flex";
+}
+
+// Close the modal
+function closeModal() {
+    document.getElementById("employeeModal").style.display = "none";
+}

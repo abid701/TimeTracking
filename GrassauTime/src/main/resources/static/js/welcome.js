198a4cd4d7040c803e13c97eb9709ef95c632090
Abid Erfan <abid_erfan7@yahoo.com>
2025-03-06 11:07:21 +0100

    Delete GrassauTime directory

--- GrassauTime/src/main/resources/static/js/welcome.js
+++
@@ -1,6 +0,0 @@
-function checkLength() {
-    var inputField = document.getElementById("cardNumber");
-    if (inputField.value.length >= 10) {
-        document.getElementById("activityLogger").submit(); // Submit the form if 10 or more characters
-    }
-}

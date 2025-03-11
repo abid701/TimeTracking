b3a6200fbbcd94e2f9ee206a5aeccee2be60b9b1
Abid Erfan <abid_erfan7@yahoo.com>
2025-03-12 00:05:50 +0100

    Update employee.js

--- GrassauTime/src/main/resources/static/js/employee.js
+++ GrassauTime/src/main/resources/static/js/employee.js
@@ -1,14 +1,14 @@
-  function toggleSettings() {
-      document.querySelector(".settings-menu").classList.toggle("show");
-  }
+function toggleSettings() {
+  document.querySelector(".settings-menu").classList.toggle("show");
+}
 
 
-  // Open the modal
-  function openModal() {
-      document.getElementById("employeeModal").style.display = "flex";
-  }
+// Open the modal
+function openModal() {
+  document.getElementById("employeeModal").style.display = "flex";
+}
 
-  // Close the modal
-  function closeModal() {
-      document.getElementById("employeeModal").style.display = "none";
-  }
+// Close the modal
+function closeModal() {
+  document.getElementById("employeeModal").style.display = "none";
+}
